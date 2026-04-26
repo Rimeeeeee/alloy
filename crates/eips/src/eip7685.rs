798dd9205240ebbf9ad44443a2a7db255aa3d240
Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>
2026-04-26 10:57:22 +0530

    derive ssz

--- crates/eips/src/eip7685.rs
+++ crates/eips/src/eip7685.rs
@@ -19,6 +19,7 @@ pub const EMPTY_REQUESTS_HASH: B256 =
 #[derive(Debug, Clone, PartialEq, Eq, Default, Hash, Deref, DerefMut, From, IntoIterator)]
 #[cfg_attr(any(test, feature = "arbitrary"), derive(arbitrary::Arbitrary))]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "ssz", derive(ssz_derive::Encode, ssz_derive::Decode))]
 pub struct Requests(Vec<Bytes>);
 
 impl Requests {
